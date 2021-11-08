5ef8958493d2b16f749f34550a46ad4b536efb0a
Marco Porto <50577988+marco-porto@users.noreply.github.com>
2021-11-08 11:15:13 +0000

    add express and others depends

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+require('dotenv').config()
 const handle_forecast_render_req = require('./lib/render_forecast.js')
 
 const express = require('express')
 const app = express();
 
 
-app.get('/forecast/:locality', (req, res) => {
+app.get('/:locality', (req, res) => {
     handle_forecast_render_req({req,res})
-    //res.send("a")
 })
 
 
-app.listen(5000, console.log('App Running On Port 5000!'))
+app.listen(process.env.EXPRESS_PORT, console.log(`Api running at http://localhost:${process.env.EXPRESS_PORT}`))   
--- src/lib/render_forecast.js
+++ src/lib/render_forecast.js
@@ -1,15 +1,25 @@
-const Regex = require("regex");
-
 function handle_forecast_render_req(args){
-    let regex = new Regex(/^[a-zA-Z ]*$/)
+    let regex = new RegExp(/^[a-zA-Z ]*$/)
     let locality = args.req.params.locality
+
+    console.log(locality,regex.test(locality))
 
     if(locality != null && locality != undefined){
         if(regex.test(locality)){
-            
-            args.res.send("hello")
+            handle_forecast_external_api_req(locality)
+
+
+
+            args.res.send("ok")
         }
     }
 
+    args.res.send("error")
 }
+
+function handle_forecast_external_api_req(locality){
+    
+}
+
+
 module.exports = handle_forecast_render_req
