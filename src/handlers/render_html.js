a6a249e09598880afd60915ed5ef8ebb3a652b63
Marco Porto <50577988+marco-porto@users.noreply.github.com>
2021-11-10 20:00:31 +0000

    Update render_html.js

--- src/handlers/render_html.js
+++ src/handlers/render_html.js
@@ -1,29 +1,7 @@
 const node_html_image = require('node-html-to-image')
 
 async function handler_render_html(params){
-    let html_string = `<html><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-      <body style="width: 440px;height: 534px;font-family: Poppins;font-style: normal;display:flex;flex-direction: column; justify-content:center;text-align: center;color:#FFFFFF;">  
-        <div style="width: 100%;height: 100%;border-radius:20px;background: radial-gradient(136.48% 153.46% at 98.7% 2.16%, rgba(4, 81, 157, 0.15) 0%, rgba(9, 85, 121, 0.15) 100%), linear-gradient(180deg, #0B0D41 0%, #060622 100%);">
-          <p style="font-weight:600;font-size:26px;text-transform:capitalize;">${params.content.weather[0].description}</p> 
-          <p style="font-weight:500;font-size:19px;color:#8B8A9A;">${params.content.name}</p>
-          <div style="display:flex;justify-content:center"><img src="${translate_weather_to_image(params.content.weather[0].id)}" style="width: 170px;height: 170px;"></div>
-          <div style="display:flex;justify-content:center">
-            <div style="padding:25px">
-              <p style="font-size:14px;line-height:10px">Temp</p>
-              <p style="font-weight:600;font-size:17px;line-height:15px">${params.content.main.temp.toFixed(1)}</p>
-            </div>
-            <div style="padding:25px">
-              <p style="font-size:14px;line-height:10px">Wind</p>
-              <p style="font-weight:600;font-size:17px;line-height:15px">${(params.content.wind.speed * 3.6).toFixed(1)} km/h</p>
-            </div>
-            <div style="padding:25px">
-              <p style="font-size:14px;line-height:10px">Humidity</p>
-              <p style="font-weight:600;font-size:17px;line-height:15px">${params.content.main.humidity}%</p>
-            </div>
-          </div>
-        </div>
-      </body>
-    </html>`
+  let html_string = `<html><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"><link href=https://fonts.googleapis.com rel=preconnect><link href=https://fonts.gstatic.com rel=preconnect crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"rel=stylesheet><body style=width:440px;height:534px;font-family:Poppins;font-style:normal;display:flex;flex-direction:column;justify-content:center;text-align:center;color:#FFF><div style="width:100%;height:100%;border-radius:20px;background:radial-gradient(136.48% 153.46% at 98.7% 2.16%,rgba(4,81,157,.15) 0,rgba(9,85,121,.15) 100%),linear-gradient(180deg,#0B0D41 0,#060622 100%)"><p style=font-weight:600;font-size:26px;text-transform:capitalize>${params.content.weather[0].description}<p style=font-weight:500;font-size:19px;color:#8B8A9A>${params.content.name}<div style=display:flex;justify-content:center><img src=${translate_weather_to_image(params.content.weather[0].id)} style=width:170px;height:170px;margin-bottom:50px;margin-top:10px></div><div style=display:flex;justify-content:center><div style=padding:25px><p style=font-size:14px;line-height:10px>Temp<p style=font-weight:600;font-size:17px;line-height:15px>${params.content.main.temp.toFixed(1)}</div><div style=padding:25px><p style=font-size:14px;line-height:10px>Wind<p style=font-weight:600;font-size:17px;line-height:15px>${(params.content.wind.speed * 3.6).toFixed(1)} km/h</div><div style=padding:25px><p style=font-size:14px;line-height:10px>Humidity<p style=font-weight:600;font-size:17px;line-height:15px>${params.content.main.humidity}%</div></div></div></body></html>`
   return await node_html_image({
     puppeteerArgs: { args: ['--no-sandbox'] },
     html: html_string,
